5d691f088d0feef287d4512d986a75f6b9483f30
gittadm <vb2freelance@yandex.ru>
2025-09-07 22:03:04 +0300

    init

---
+++ app/Http/Controllers/HelloController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class HelloController extends Controller
+{
+    public function show()
+    {
+        return 1;
+    }
+
+    public function task2()
+    {
+        $a = [1, 2];
+
+        return 1;
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\Http\Controllers\HelloController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('hello', [HelloController::class, 'show']);
+Route::get('task1', [HelloController::class, 'task1']);
+
+Route::get('/', function () {
+    return view('welcome');
+});
